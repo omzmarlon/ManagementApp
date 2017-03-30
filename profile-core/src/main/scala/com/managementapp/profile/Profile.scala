6d1166edc2a5618d8cec9597339ceca1688918b8
Marlon Ou <omzmarlon@gmail.com>
2017-03-29 21:57:17 -0700

    some stub files

---
+++ profile-core/src/main/scala/com/managementapp/profile/Profile.scala
@@ -0,0 +1,8 @@
+package com.managementapp.profile
+
+/**
+  * Created by omzmarlon on 2017-03-29.
+  */
+class Profile {
+
+}
---
+++ quiz-core/src/main/scala/com/managementapp/quiz/Quiz.scala
@@ -0,0 +1,8 @@
+package com.managementapp.quiz
+
+/**
+  * Created by omzmarlon on 2017-03-29.
+  */
+class Quiz {
+
+}
